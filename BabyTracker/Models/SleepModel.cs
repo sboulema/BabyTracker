0fa5c3a38a29e657ba403e388cb2e223e88293d6
Samir L. Boulema <sboulema@gmail.com>
2020-07-24 23:56:59 +0200

    Pretty format duration for Sleep

--- BabyTracker/Models/SleepModel.cs
+++ BabyTracker/Models/SleepModel.cs
@@ -3,5 +3,25 @@
     public class SleepModel : EntryModel
     {
         public string Duration { get; set; }
+
+        public string DurationText 
+        {
+            get 
+            {
+                var success = int.TryParse(Duration, out var durationInMins);
+
+                if (!success) 
+                {
+                    return string.Empty;
+                }
+
+                if (durationInMins < 60) 
+                {
+                    return $"{durationInMins} min";
+                }
+                
+                return $"{(durationInMins / 60)} hrs {durationInMins % 60} min";
+            }
+        }
     }
 }
