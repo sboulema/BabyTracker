39e0a244c1917b767576bed2ebb88da6589a859a
Samir Boulema <s.boulema@louwmangroup.nl>
2021-11-10 20:27:43 +0100

    Auth0 cookie change

--- BabyTracker/Startup.cs
+++ BabyTracker/Startup.cs
@@ -2,6 +2,7 @@ using Auth0.AspNetCore.Authentication;
 using BabyTracker.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -37,6 +38,11 @@ public class Startup
         });
 
         services.AddMjmlServices();
+
+        services.Configure<CookiePolicyOptions>(options =>
+        {
+            options.MinimumSameSitePolicy = SameSiteMode.None;
+        });
 
         services.AddAuth0WebAppAuthentication(options => {
             options.Domain = Configuration["AUTH0_DOMAIN"];
