019844b07a16f2ace2cacec773e31df7c696da29
sboulema <sboulema@gmail.com>
2024-10-21 11:29:32 +0200

    Add CacheController

---
+++ src/Controllers/CacheController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.OutputCaching;
+using System.Threading;
+
+namespace BabyTracker.Controllers;
+
+[Route("[controller]")]
+public class CacheController(
+	IOutputCacheStore outputCacheStore) : Controller
+{
+	[Authorize]
+	[HttpGet("[action]")]
+	public async Task<IActionResult> Clear(CancellationToken cancellationToken)
+	{
+		await outputCacheStore.EvictByTagAsync(User.FindFirstValue(ClaimTypes.NameIdentifier), cancellationToken);
+		
+		return RedirectToAction("Index", "Home");
+	}
+}
